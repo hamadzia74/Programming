4e59b3155a8e4d84c05373ebbb18dddebb0fdb79
Hamad Zia <77169231+hamadzia74@users.noreply.github.com>
2022-08-02 23:40:55 +0500

    Add files via upload

---
+++ C++/swapping values with variable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=10,b=5,c;
+cout<<"before swapping."<<endl;
+cout<<"a="<<a<<",b="<<b<<endl;
+a=c;
+b=a;
+cout<<"\nafter swapping."<<endl;
+cout<<"a="<<a<<",b="<<b<<endl;
+
+	return 0;	
+}
